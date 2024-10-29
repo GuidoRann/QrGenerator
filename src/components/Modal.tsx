e8605d8c94f1911cb0bf8d76b4d402339dc6e8f9
Guido Rann <guidorann.code@gmail.com>
2024-10-29 20:46:16 -0300

    fix: fix deploy error

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -11,12 +11,12 @@ export default function Modal({ isOpen, onClose, qrImageUrl }: modalProps) {
     <>
     {isOpen && (
       <div className="animate-fade-in animate-duration-300 fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex flex-col justify-center items-center text-gray-200">
-        <section className="bg-[#3c3a45] w-[90%] flex flex-col items-center rounded-xl">
+        <section className="bg-gray-800 w-[90%] flex flex-col items-center rounded-xl">
         <button className="w-full flex justify-end px-2 py-3 font-black" onClick={onClose}><X size={25}/></button>
           <img className="w-[84%] rounded-[30px]" src={qrImageUrl} alt="QR Code" />
           <div className="py-4">
             <button
-              className="flex justify-center bg-purple-600 hover:bg-purple-700 px-12 py-3 md:px-7 md:py-3 cursor-pointer rounded-xl font-medium hover:scale-110 duration-300"
+              className="flex justify-center bg-primary-color hover:bg-secondary-color px-12 py-3 md:px-7 md:py-3 cursor-pointer rounded-xl font-medium hover:scale-110 duration-300"
               onClick={onClose}
             >
               <a href={qrImageUrl} download="qr-code">Descargar QR</a>
